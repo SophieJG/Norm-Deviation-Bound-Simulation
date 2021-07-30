fc4c5563a4543675845c07ceb294852fb5593659
SophieJG <sjwood1515@gmail.com>
2021-07-30 12:11:03 -0700

    Make minor adjustments

--- PlotScript.m
+++ PlotScript.m
@@ -2,6 +2,7 @@ clc;
 
 FILE_NAME = 'simulationPlot';
 DATA_FILE = 'Regression/simulationData.csv';
+SCALE = 0.791;
 
 T = readtable(DATA_FILE);
-generatePlots(T.p, T.K, T.KsqrtlogK, T.deviation, FILE_NAME, true);
+generatePlots(T.p, T.K, SCALE*T.KsqrtlogK, T.deviation, FILE_NAME, true);
